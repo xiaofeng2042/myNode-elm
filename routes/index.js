ddb7cad68aca15123d4da58ef4aba4e175328b22
zxc <601244325@qq.com>
2018-01-26 11:26:58 +0800

    实时监控变化

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express222' });
+  res.render('index', { title: 'Express2233' });
 });
 
 module.exports = router;
