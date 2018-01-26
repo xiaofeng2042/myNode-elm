e7d19216bcae976357463cf809f40f6659dbe756
zxc <601244325@qq.com>
2018-01-26 14:05:39 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express223344' });
+  res.render('index', { title: 'Express22334455' });
 });
 
 module.exports = router;
